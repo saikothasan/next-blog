f3b1f3240fa6f20998f11960ea96ded7dc86b24e
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-01-28 22:02:59 +0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -90,6 +90,6 @@ const config: Config = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
 };
 export default config;
